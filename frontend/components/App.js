1f234907951842c76b2842e53812637fa494798e
Rylie <rylie.stone2005@gmail.com>
2023-12-13 11:16:52 -0500

    transfered data from api to webpage

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react'
 import React from 'react'
+import axios from 'axios'
+import { Await } from 'react-router-dom'
 
 function App() {
-  return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
-  )
-}
+  function Card ({title, text, IMGurl, date}) {
+    return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{date}</h3>
+      <img src={IMGurl}></img>
+      <p>{text}</p>
+    </div>
+    )
+  }
 
+  const [picture, setPicture] = useState()
+    
+  useEffect(() => {
+      axios.get('http://localhost:9009/api/apod?api_key=DEMO_KEY')
+      .then(data => {setPicture(data.data)})
+      .catch(err => console.error(err))
+    }, [])
+    return (
+      picture ? 
+      <Card 
+      title={picture.title}
+      date={picture.date}
+      IMGurl={picture.url}
+      text={picture.explanation}
+      />
+      
+      : <h1>Loading...</h1>
+    )
+}
 export default App
