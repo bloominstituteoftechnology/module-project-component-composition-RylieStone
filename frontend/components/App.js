63ee6c450add448801b37f9683b41dc8ff40cc81
Rylie <rylie.stone2005@gmail.com>
2023-12-18 08:21:27 -0500

    finished day 2

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,35 @@
 import { useEffect, useState } from 'react'
 import React from 'react'
 import axios from 'axios'
+import { styled } from 'styled-components'
 
 function App() {
+  const Styled = styled.div`
+  h3 {
+    display: flex;
+    justify-content: center;
+    font-family: Copperplate, Papyrus;
+  }
+  p {
+    font-family: "Lucida Handwriting", "Brush Script MT";
+    color: blue;
+  }
+  img {
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    border: 1px solid black;
+  }
+  `
+
   function Card ({title, text, IMGurl, date}) {
     return (
-    <div>
+    <Styled>
       <h2>{title}</h2>
       <h3>{date}</h3>
       <img src={IMGurl}></img>
       <p>{text}</p>
-    </div>
+    </Styled>
     )
   }
 
